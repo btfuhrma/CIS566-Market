45874e7a636683906777395b02454dc69d9e8b7e
btfuhrma <btfuhrma@mtu.edu>
2024-10-24 15:33:08 -0400

    created initial homepage, started creating search and create items

--- Market/Item/views.py
+++ Market/Item/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Item
 # Create your views here.
 
 def index(request):
     return render(request, "Item/index.html")
 
-def search(request, title):
-    context = "temp"
-    return render(request, "Item/index.html")
+def search(request):
+    if request.method == "GET":
+        title = request.GET.get('title', '')
+        results = Item.objects.filter(title__icontains=title)
+
+    context = {'results' : results,
+               'title': title
+            }
+
+    return render(request, "Item/search.html", context)
+
+def createItem(request):
+    return render(request, "Item/createItem.html")
+
+def create(request):
+    pass
