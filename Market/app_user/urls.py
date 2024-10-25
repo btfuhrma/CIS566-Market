c750ec108e61032322e4b4938a5d5ff0cf084733
btfuhrma <btfuhrma@mtu.edu>
2024-10-25 00:39:08 -0400

    logout and login functionality working

--- Market/app_user/urls.py
+++ Market/app_user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,5 +7,6 @@ urlpatterns = [
     path("createAccount", views.createAccount, name="createAccount"),
     path("login", views.login, name="login"),
     path("createUser", views.createUser, name="createUser"),
-    path("loginPage", views.loginPage, name="loginPage")
+    path("loginPage", views.loginPage, name="loginPage"),
+    path('logout/', views.logoutUser, name='logout'),
 ]
--- Market/app_user/views.py
+++ Market/app_user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import AppUser
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib import messages
+from django.contrib.auth import logout
 
 # Create your views here.
 def createAccount(request):
@@ -21,7 +22,7 @@ def login(request):
         if user is not None:
             # Log the user in
             auth_login(request, user)
-            return redirect('home')  # Redirect to a home or dashboard page
+            return render(request, template_name="Item/index.html")  # Redirect to a home or dashboard page
         else:
             # Invalid login details
             messages.error(request, "Invalid email or password.")
@@ -36,3 +37,7 @@ def createUser(request):
         userManager = AppUser.objects
         userManager.create_user(email=email, password=password)
     return render(request, "app_user/login.html")
+
+def logoutUser(request):
+    logout(request)
+    return render(request, "Item/index.html")
